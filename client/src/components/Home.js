43614ddd2f7075fb151fe3e83ec33e2e72ad28ee
Aaron_Winn <winn403@hotmail.com>
2018-01-06 12:05:16 -0700

    isucktoday

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Header, Segment } from 'semantic-ui-react';
+import { Header, Card } from 'semantic-ui-react';
 
 class Home extends Component {
   render() {
@@ -17,6 +18,22 @@ class Home extends Component {
           <Grid.Column width={2}/>
         </Grid.Row>
       </Grid>    
+      <Div>
+        <Header as='h1' textAlign='center'>Home Component</Header>
+        <p>In the heart of downtown Salt Lake City, against the backdrop of the Wasatch Mountains, a unique
+           culinary and cultural experience awaits you. Traditional Nepali and Indian Cuisine is prepared daily
+            with the freshest ingredients from scratch by dedicated Nepali chefs.  We welcome you to walk through
+             our doors and experience the mouth-watering aroma and flavors of our curries, warm hand made naan, 
+             sizzling Tandoori grills, organic Himalayan coffee and more. All served in an inviting atmosphere by 
+             our friendly staff. Namaste! 
+
+              Our Everest Hall is currently showing the beautiful traditional "Mithila Art" from the women's
+               collective in Janukpur. For more info, visit http://mithila-art.blogspot.com/ Please visit us regularly
+                and join our mailing list for special events and announcements.</p>
+      </Div>
+
+       
+
     );
   }
 }
