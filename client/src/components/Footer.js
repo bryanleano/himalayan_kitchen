9631b72e3bae72c3cb3e75ea491d2fdde7ca8e11
Bryan Leanoe <bryan.leano@gmail.com>
2018-01-06 15:42:55 -0700

    fix footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="ui inverted vertical footer segment" style={{ backgroundColor: "#f0b100"}}>
-    <div className="ui center aligned container">
-      <h4 className="ui inverted header">&copy; Copyright 2018 | All rights reserved | JAABI Studios</h4>
-      <a href="https://www.facebook.com/"><i className="facebook square icon big"></i></a>
-      <a href="https://twitter.com/"><i className="twitter square icon big"></i></a>
-      <a href="https://instagram.com/"><i className="instagram square icon big"></i></a>
-      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><i className="youtube square icon big"></i></a>
-      <a href="https://www.linkedin.com/in/bryan-leano"><i className="linkedin square icon big"></i></a>
-    </div>
+  <div class="ui inverted vertical footer segment" style={styles.footer}>
+  <div class="ui center aligned container">
+      <h4 class="ui inverted header">&copy; Copyright 2018 | All rights reserved | JAABI Studios</h4>
+      <a href="https://www.facebook.com/"><i class="facebook square icon big"></i></a>
+      <a href="https://twitter.com/"><i class="twitter square icon big"></i></a>
+      <a href="https://instagram.com/"><i class="instagram square icon big"></i></a>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><i class="youtube square icon big"></i></a>
+      <a href="https://www.linkedin.com/in/bryan-leano"><i class="linkedin square icon big"></i></a>
+  </div>
   </div>
 )
+
+const styles = {
+  footer: {
+    backgroundColor: "#f0b100",
+    bottom: '0px',
+    left: '0px',
+    right: '0px',
+    marginTop: '20px'
+  }
+}
 
 export default Footer;
